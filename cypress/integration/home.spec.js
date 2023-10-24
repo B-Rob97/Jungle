0916b4b57f0276e0c6961b270d9654e7c3c69c68
Brett Roberts <brett.roberts@live.ca>
2023-10-24 17:52:47 -0600

    Test home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+describe('Homepage tests', () => {
+
+beforeEach(() => {
+  cy.visit('/')
+
+})
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+})
