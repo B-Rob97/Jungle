e1027928c1b79b3067424c6cf032f895b0cd19f3
Brett Roberts <brett.roberts@live.ca>
2023-10-23 23:25:42 -0600

    refactor

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,25 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # Create category and product before each test
     before(:each) do
-      @category = Category.create(name: 'Electronics') # Assuming Category is a model you have
+      @category = Category.create(name: 'Electronics')
       @product = Product.new(name: 'Sample Product', price: 100, quantity: 10, category: @category)
     end
 
-    # Test that a product with all fields set will save successfully
     it 'should save successfully with all four fields set' do
       expect(@product).to be_valid
     end
 
-    # Test that a product without a name will not save and will produce the correct error message
     it 'should not save without a name' do
       @product.name = nil
       @product.save
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
-    # Test that a product without a price will not save and will produce the correct error message
     it 'should not save without a price' do
       @product.price_cents = nil
       @product.save
@@ -28,15 +24,12 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include("Price is not a number")
     end
 
-
-    # Test that a product without a quantity will not save and will produce the correct error message
     it 'should not save without a quantity' do
       @product.quantity = nil
       @product.save
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
-    # Test that a product without a category will not save and will produce the correct error message
     it 'should not save without a category' do
       @product.category = nil
       @product.save
